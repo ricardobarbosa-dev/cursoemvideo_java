874015f90a4f3cb77b0098c8b4d59d9c83e0dda6
Ricardo Barbosa <ricardos-barbosa@hotmail.com>
2026-09-04 13:20:16 -0300

    Added all files in folder correct.

---
+++ java_basico/ResolucaoMonitor.java
@@ -0,0 +1,14 @@
+package java_basico;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class ResolucaoMonitor {
+    public static void main(String[] args) {
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension dimensao = tk.getScreenSize();
+        int largura = dimensao.width;
+        int altura = dimensao.height;
+        System.out.println("A resolução da tela atual é: " + largura + " x " + altura);
+    }
+}
