af7910cd7533fdc550f8df0b4113f8116454713b
Ricardo Barbosa <ricardos-barbosa@hotmail.com>
2026-09-04 14:00:07 -0300

    Added files in folder java_basico

---
+++ java_basico/IdiomaDaMaquina.java
@@ -0,0 +1,11 @@
+package java_basico;
+import java.util.Locale;
+
+public class IdiomaDaMaquina {
+    public static void main(String[] args) {
+        Locale idioma = Locale.getDefault(); // nosso o
+        System.out.println("Idioma do sistema: ");
+        System.out.println(idioma.getDisplayName(idioma));
+    }
+}
+ 
