f614507353fbdf6d3ff0415b369349eb12f5bde4
Ricardo Barbosa <ricardos-barbosa@hotmail.com>
2026-09-05 14:16:32 -0300

    Comment added

--- java_basico/IdiomaDaMaquina.java
+++ java_basico/IdiomaDaMaquina.java
@@ -3,7 +3,7 @@ import java.util.Locale;
 
 public class IdiomaDaMaquina {
     public static void main(String[] args) {
-        Locale idioma = Locale.getDefault(); // nosso o
+        Locale idioma = Locale.getDefault(); // nosso idioma do sistema operacional
         System.out.println("Idioma do sistema: ");
         System.out.println(idioma.getDisplayName(idioma));
     }
