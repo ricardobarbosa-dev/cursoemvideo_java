45c61492c8a70197e9810b46292d9c4bf36ba35d
Ricardo Barbosa <ricardos-barbosa@hotmail.com>
2026-09-05 23:03:42 -0300

    Projects added

---
+++ OlaMundoJavaFX/src/olamundojavafx/FXMLDocumentController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package olamundojavafx;
+
+import java.awt.Button;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author rickswoosh
+ */
+public class FXMLDocumentController implements Initializable {
+     
+   @FXML
+    private Label lblMensagem;
+    private Button btnClick;
+    
+    @FXML
+    private void clicouBotao(ActionEvent event) {
+        lblMensagem.setText("Você entrou no sistema!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
